dc2095c700c3c6381dc517dff576f4007890dcba
Osekobo <collinsboseko2005@gmail.com>
2025-10-29 21:57:20 +0300

    save changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD:vite.config.js
   base: '/the-Farm/', 
 })
+=======
+  optimizeDeps: {
+    include: ['@reduxjs/toolkit', 'react-redux'],
+  },
+});
+>>>>>>> 11a0331 (save changes):the-farm/vite.config.js
