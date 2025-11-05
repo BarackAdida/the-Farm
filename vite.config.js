ce098b9bc0aa8883ea93a5a1724200fedb186c8c
BarackAdida <adida.barack@student.moringaschool.com>
2025-11-05 23:30:16 +0300

    new

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD:vite.config.js
   base: '/the-Farm/', 
 })
-=======
-  optimizeDeps: {
-    include: ['@reduxjs/toolkit', 'react-redux'],
-  },
-});
->>>>>>> 11a0331 (save changes):the-farm/vite.config.js
+
+  
